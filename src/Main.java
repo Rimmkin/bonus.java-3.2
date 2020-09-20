7b51fca5a7dcb498fa2f4d0181cbacb6d2c544f7
Rimma Stolina <rim2290@mail.ru>
2020-09-20 14:35:49 +0300

    srcadded

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        int price = 7800;
-        int amount = 20;
-        int bonus = price / amount;
-        System.out.println(bonus);
+public class Main
+{public static void main(String[] args) {
+    int score = 2000;
+    int transfer = 10000;
+    int bonus = 0;
+     if (transfer>1000){
+        bonus = transfer/100;
+    }
+    int sum = (score + transfer)/100 + bonus;
+    System.out.println(bonus);
+    System.out.println(sum);
     }
 }
