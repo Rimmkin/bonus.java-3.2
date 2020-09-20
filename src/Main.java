db95515feeaf373394e98adc52f574037e818bff
Rimma Stolina <rim2290@mail.ru>
2020-09-20 14:16:06 +0300

    src added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7800;
+        int amount = 20;
+        int bonus = price / amount;
+        System.out.println(bonus);
+    }
+}
